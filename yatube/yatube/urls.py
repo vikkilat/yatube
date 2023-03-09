404d760015811eb95ac51b3437e4039a30c408cf
Виктория Латышева <vikkilat1989@yandex.ru>
2023-03-09 19:18:38 +0300

    Убрала статику из gitignore

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('posts.urls')),
@@ -13,9 +13,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
 
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
